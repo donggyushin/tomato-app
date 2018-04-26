a2fa09dafad1f7bd71c53fb425eaaa1f0b294dac
donggyu <34573243+donggyushin@users.noreply.github.com>
2018-04-26 16:42:29 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Timer from "./components/Timer";
+import reducer from "./reducer";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+let store = createStore(reducer);
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Timer />
+      </Provider>
+    );
+  }
+}
